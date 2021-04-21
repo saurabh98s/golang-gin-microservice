3bb4ca72d03a980b5323b697824a2963e307ae78
Saurabh suman <geek.saurabh98@gmail.com>
2021-04-22 00:40:05 +0530

    add:users struct

--- controllers/users/user_controller.go
+++ controllers/users/user_controller.go
@@ -1,13 +1,27 @@
 package users
 
 import (
+	"encoding/json"
+	"fmt"
+	"golang-gin-microservice/domain/users"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
+	var users users.User
+	fmt.Println(users)
+	bytes, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		// TODO: Handle Error
+	}
+	if err := json.Unmarshal(bytes, &users); err != nil {
 
+	}
+	fmt.Println(string(bytes))
+	fmt.Println(err)
 	c.JSON(http.StatusNotImplemented, "implement me!")
 
 }
